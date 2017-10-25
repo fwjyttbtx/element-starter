be3cef4422855a7625087a6db0496b4d7cef449a
weijiefu <jieweifu@126.com>
2017-10-25 17:14:28 +0800

    修改为默认模板

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routers'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-import App from './App.vue'
+import VueMoment from 'vue-moment'
 
-Vue.use(ElementUI)
+import App from './App.vue'
+import axios from 'axios'
+
+Vue.use(VueRouter);
+Vue.use(ElementUI);
+Vue.use(VueMoment);
+
+Vue.prototype.$http = axios;
+
+const router = new VueRouter({routes, history: true});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-})
+});
